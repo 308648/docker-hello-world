26f98f4de93d5c6ccd52f202b3bf3934e2a1b3e4
308648 <50056640+308648@users.noreply.github.com>
2019-04-27 21:35:25 +0200

    Added hello world express server

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
 
-let message : string = "Hello World";
-console.log(message);
+import * as express from 'express';
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!');
+});
